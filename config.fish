90a2dd417698a69144cc1aef1a40930cfa4f8c9e
Robbie N. P <109235166+halfcodes@users.noreply.github.com>
2022-08-19 18:37:15 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,12 @@
-# remove the greeting
+# clear the greeting
 set fish_greeting ""
+# disable autosuggestions
+set -g fish_autosuggestion_enabled 0
+
 
 # aliases
 alias lr "ls -rtla"
-alias vim nvim
+alias vim "nvim"
 
 # homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
